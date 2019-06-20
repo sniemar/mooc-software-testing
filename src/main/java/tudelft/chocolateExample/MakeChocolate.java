db7c7bcd7d0b2bff1a443c3d6a76b1975ce3d223
sniemar <nie.mar@yandex.com>
2019-06-20 17:07:54 +0300

    adding MakeChocolate and tests for it

---
+++ src/main/java/tudelft/chocolateExample/MakeChocolate.java
@@ -0,0 +1,12 @@
+package tudelft.chocolateExample;
+
+public class MakeChocolate {
+    public int makeChocolate(int small, int big, int goal) {
+        int maxBigBoxes = goal / 5;
+        int bigBoxesWeCanUse = maxBigBoxes < big ? maxBigBoxes : big;
+        goal -= (bigBoxesWeCanUse * 5);
+        if(small <= goal)
+            return -1;
+        return goal;
+    }
+}
