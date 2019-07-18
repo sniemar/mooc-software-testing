7fb9d47e71fcb2ea1c9bd79c767449c289a34f5d
sniemar <nie.mar@yandex.com>
2019-07-18 19:28:36 +0300

    fix string doubling

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,19 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class CaesarShiftCipherTest {
 
+    @ParameterizedTest(name = "{0}: {1}, {2} = {3}")
+    @CsvSource({
+            "'l', 'abc', 3, 'def'",
+            "'2', 'zef', 3, 'chi'",
+    })
+
+    public void tests (String partition, String str, int shift, String expected) {
+        String result = (new CaesarShiftCipher()).CaesarShiftCipher(str, shift);
+        Assertions.assertEquals(expected, result);
+    }
 }
